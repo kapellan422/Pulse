3f3aa717759c111d139934448b878005f0c05fc2
Mikhail <kapellan422@gmail.com>
2022-10-06 19:58:38 +0600

    add and style slider

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevrons/chevron_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevrons/chevron_right.png"></button>',
+    });
+  });
